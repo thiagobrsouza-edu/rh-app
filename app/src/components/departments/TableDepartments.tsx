916bd0be35382c54ec457e73e47b9536096cc04c
thiagobrsouza-edu <thiago.glespaul@gmail.com>
2022-03-01 16:50:44 -0300

    Creating a filter for departmet table

--- app/src/components/departments/TableDepartments.tsx
+++ app/src/components/departments/TableDepartments.tsx
@@ -1,6 +1,7 @@
 import { AxiosResponse } from "axios";
+import Link from "next/link";
 import Router from "next/router";
-import React, { useEffect, useState } from "react";
+import React, { ChangeEvent, useEffect, useState } from "react";
 import { ToastContainer } from "react-toastify";
 import { useDepartment } from "../../api/departments/DepartmentService";
 import { IDepartment } from "../../api/departments/IDepartment";
@@ -9,6 +10,7 @@ export const TableDepartments: React.FC = () => {
 
   const service = useDepartment();
   const [departments, setDepartments] = useState<IDepartment[]>([]);
+  const [filter, setFilter] = useState('');
 
   useEffect(() => {
     service.list().then((response: AxiosResponse) => {
@@ -31,36 +33,65 @@ export const TableDepartments: React.FC = () => {
     }
   }
 
+  /**
+   * filter configuration
+   */
+  const filterDepartments = (department: IDepartment) => {
+    const lowerFilter = filter.toLowerCase();
+    return (
+      department.description?.toString().toLowerCase().includes(lowerFilter)
+    );
+  }
+
+  const onFilterChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setFilter(event.target.value);
+  }
+
+  const departmentsFiltered = departments.filter(filterDepartments);
+
   return (
-    <div className="mt-4 overflow-auto" style={{ height: "20em" }}>
-      <table className="table table-hover">
-        <thead>
-          <tr>
-            <th>Código</th>
-            <th>Descrição</th>
-            <th>Ações</th>
-          </tr>
-        </thead>
-        <tbody>
-          {
-            departments.map((department: IDepartment) => (
-              <tr key={department.id}>
-                <td>{department.id}</td>
-                <td>{department.description}</td>
-                <td>
-                  <button className="ms-1 btn btn-primary" onClick={e => updateSelect(department)}>
-                    <i className="bi bi-pencil-square"></i>
-                  </button>
-                  <button className="ms-1 btn btn-danger" onClick={e => deleteSelect(department)}>
-                    <i className="bi bi-trash-fill"></i>
-                  </button>
-                </td>
-              </tr>
-            ))
-          }
-        </tbody>
-      </table>
+    <>
+      <div className="row justify-content-center">
+        <Link href={'/departments/add'}>
+          <button className="mt-2 col-md-3 col-sm-4 col-xs-12 btn btn-primary">
+            Novo Departamento <i className="bi bi-plus-circle"></i>
+          </button>
+        </Link>
+        <div className="mt-2 col-md-6 col-sm-8 col-xs-12">
+          <input type="text" className="form-control" placeholder="Pesquise por descrição"
+            value={filter} onChange={onFilterChange} />
+        </div>
+      </div>
+      <div className="mt-2 overflow-auto" style={{ height: '20em' }}>
+        <table className="mt-2 table table-hover">
+          <thead>
+            <tr>
+              <th>Código</th>
+              <th>Descrição</th>
+              <th>Ações</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              departmentsFiltered.map((department: IDepartment) => (
+                <tr key={department.id}>
+                  <td>{department.id}</td>
+                  <td>{department.description}</td>
+                  <td>
+                    <button className="ms-1 btn btn-primary" onClick={e => updateSelect(department)}>
+                      <i className="bi bi-pencil-square"></i>
+                    </button>
+                    <button className="ms-1 btn btn-danger" onClick={e => deleteSelect(department)}>
+                      <i className="bi bi-trash-fill"></i>
+                    </button>
+                  </td>
+                </tr>
+              ))
+            }
+          </tbody>
+        </table>
+      </div>
       <ToastContainer />
-    </div>
+    </>
   )
 }
--- app/src/pages/departments/index.tsx
+++ app/src/pages/departments/index.tsx
@@ -1,14 +1,10 @@
 import { NextPage } from "next";
-import Link from "next/link";
 import { TableDepartments } from "../../components/departments/TableDepartments";
 import { Layout } from "../../components/layout/Layout";
 
 const Departments: NextPage = () => {
   return (
     <Layout title="Controle de Departamentos">
-      <Link href={'/departments/add'}>
-        <button className="mt-3 btn btn-primary">Novo Departamento</button>
-      </Link>
       <TableDepartments />
     </Layout>
   )
